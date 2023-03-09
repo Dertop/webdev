1b30bed3e0a0ca087990342696c37d6835e2c732
Dertop <99186380+Dertop@users.noreply.github.com>
2023-03-10 00:06:07 +0600

    lab

---
+++ lab6/src/app/products.ts
@@ -0,0 +1,110 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  link: string;
+  rating: number;
+  image: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Apple AirPods 3',
+    price: 96275,
+    rating: 5,
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h12/h12/46719106023454/apple-airpods-3-belyj-102667744-1.jpg',
+    link: 'https://kaspi.kz/shop/p/apple-airpods-3-belyi-102667744/?c=750000000#!/item',
+    description: 'The dynamic driver developed by Apple uses a special amplifier, providing incredible sound detail. You will clearly hear the entire spectrum — from deep, rich bass to crystal clear high notes. The built-in microphones in each earphone are covered with a special acoustic grid. This allows you to minimize the noise from the wind during calls, so that you can always be clearly and clearly heard. The new push sensor supports even more control options: turn on and stop playback, rewind tracks, answer calls and end them. The earphone case is easy to charge with a MagSafe charger.3 When the case is fully charged, the headphones work for up to 30 hours in a row.4 And if the AirPods are completely discharged, just five minutes in the case is enough — and you can listen to music for about an hour.'
+  },
+  {
+    id: 2,
+    name: 'Apple AirPods Pro 2nd generation',
+    price: 119545,
+    rating: 5,
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hba/hf8/62281477259294/apple-airpods-pro-2nd-generation-belyj-106362968-1.jpg',
+    link: 'https://kaspi.kz/shop/p/apple-airpods-pro-2nd-generation-belyi-106362968/?c=750000000#!/item',
+    description: 'AirPods Pro are unique headphones with active noise reduction technology that continuously optimize the sound taking into account the shape of your ear and the position of the earbuds. They perfectly drown out external noises, so you can completely immerse yourself in the music. These are headphones of a completely new class. AirPods Pro are exceptionally comfortable, they have active noise reduction technology, and you can even choose earbuds of your own size. You will feel the music better, not the headphones. The set includes three pairs of soft silicone earbuds, which are easily fixed on the earphone. A special test will help you choose the size of the earbuds for optimal fit and better sound. There is a special conical cavity inside each earbud, which helps it adapt better to the geometry of the ear and hold the earphone more securely. And the holes in the earbuds allow you to equalize the pressure, so the headphones will be for you. With periodic recharging in the MagSafe case, the headphones can work for more than 24 hours. The case is compatible with various chargers — including those that support MagSafe technology, which makes wireless charging faster and more convenient. When the battery runs out, AirPods Pro sends a notification about it to your iPhone.'
+  },
+  {
+    id: 3,
+    name: 'JBL Tune 510BT',
+    price: 25505,
+    rating: 5,
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h3d/h1d/33957524537374/jbl-tune-510bt-cernyj-101420081-1-Container.jpg',
+    link: 'https://kaspi.kz/shop/p/jbl-tune-510bt-chernyi-101420081/?c=750000000#!/item',
+    description: 'JBL Tune 510BT headphones allow you to enjoy powerful bass thanks to JBL Pure Bass technology without wires. These easy-to-use headphones work up to 40 hours on a single battery charge, and by connecting the USB-C charging cable to the power supply for just five minutes, you will get another 2 hours of playback. If you receive a call while you are watching a video on another device, the JBL Tune 510BT will automatically switch to your mobile device. Created for complete comfort, the JBL Tune 510BT headphones are equipped with Bluetooth 5.0 and allow you to connect to Siri or Google without using a mobile device. The JBL Tune 510BT headphones are available in several bright colors and fold easily for easy carrying. This is a universal solution that allows you to enjoy music all day long.'
+  },
+  {  
+    id: 4,
+    name: 'Samsung Galaxy Buds 2',
+    price: 39936,
+    rating: 5,
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h20/h54/46961212391454/samsung-galaxy-buds-2-sm-r177nzkacis-chernyi-102046373-1.jpg',
+    link: 'https://kaspi.kz/shop/p/samsung-galaxy-buds-2-sm-r177nzkacis-chernyi-102046373/?c=750000000#!/item',
+    description: 'Create your own world with Galaxy Buds2. Advanced two-way acoustics, active noise reduction and thoughtful design of headphones for the enjoyment of listening to your favorite music and their comfortable use throughout the day. Immerse yourself in the atmosphere of daily workouts, games and high-quality music with convenient controls and extensive connectivity to other devices. Enjoy music with Galaxy Buds2 as if you are in a concert hall. The two-way speaker system provides a clear, balanced sound with deep bass and crystal high tones. An adaptive system of three microphones and a bone conduction sensor helps to transmit voice speech as clearly as possible, and noise reduction technology based on machine learning algorithms filters out unwanted noise and provides comfortable communication with family and friends. The design with a small protrusion minimizes the contact of the headphone case with the wind, which allows you to communicate without interference even in windy weather. Two microphones capture ambient sounds, and active noise reduction technology removes up to 98% of external noise. Listen to your favorite music, podcasts and other content without being distracted by extraneous sounds. Galaxy Buds2 will give you indescribable audio impressions for every taste.'
+  },
+  {  
+    id: 5,
+    name: 'JBL T110',
+    price: 4289,
+    rating: 5,
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h4a/h56/31569186521118/jbl-t110-cernyj-4802159-1-Container.jpg',
+    link: 'https://kaspi.kz/shop/p/jbl-t110-chernyi-4802159/?c=750000000#!/item',
+    description: 'Inside the robust housing of the earbuds is a pair of 9 mm speakers capable of producing very powerful bass and reproducing the sound of JBL Pure Bass, which can be heard in concert halls, sports arenas and recording studios around the world. In addition, a one-button control panel on a non-tangled flat cable allows you to control music playback, as well as answer calls “on the go” using the built-in microphone. All this makes the JBL T110 a convenient companion for everyday use at work, at home and on the road.'
+  },
+  {  
+    id: 6,
+    name: 'HyperX Cloud Stinger Core',
+    price: 19680,
+    rating: 5,
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h0f/h80/32936512290846/hyperx-cloud-stinger-core-chernyi-100430429-1.jpg',
+    link: 'https://kaspi.kz/shop/p/hyperx-cloud-stinger-core-chernyi-100430429/?c=750000000#!/item',
+    description: 'HyperX Cloud Stinger Core is a lightweight and durable gaming headset at a reasonable price. The 40mm speakers provide full immersion in the game, emphasizing the mid and high frequencies and highlighting the powerful bass. The microphone with noise reduction function allows you to improve the sound quality when talking with friends, eliminating extraneous noise and making your voice clearer. In both the wired and wireless versions, it is possible (optionally) to add virtual surround sound 7.11, created by NGENUITY. Surround sound takes the process of immersion into the game to a new level, helping to more accurately determine the position of enemies by sound. The wireless version allows you to save time on connecting cables without worrying about sound delay. No matter which option you choose, Cloud Stinger Core remains a reliable gaming headset, always ready for daily play.'
+  },
+  {  
+    id: 7,
+    name: 'Sony WH-1000XM4',
+    price: 199866,
+    rating: 5,
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h4a/h11/32844899811358/sony-wh-1000xm4b-cernyj-100471997-1-Container.jpg',
+    link: 'https://kaspi.kz/shop/p/sony-wh-1000xm4-chernyi-100471997/?c=750000000#!/item',
+    description: 'Find out how the WH-1000XM4 headphones combine our best noise reduction technology, exceptional sound quality and a range of intelligent features. Whether you are flying on a long trip or relaxing in a cafe, the WH-1000XM4 headphones will perfectly suppress noise and transmit high- and medium-frequency sound as efficiently as possible. The Dual Noise Sensor technology captures ambient noise using four microphones on the headphone bowls and transmits data to the QN1 HD processor, which performs noise reduction in real time, taking into account the acoustic characteristics of the environment. With a new single-chip audio system and Bluetooth® support, it evaluates the parameters of music and noise, acoustic characteristics of the space between the speakers and the auricles at a frequency of 700 times per second and adjusts the sound signal accordingly. For the most effective noise reduction, two developments are used in the WH-1000XM4 headphones: the technology of personal noise reduction optimization and the atmospheric pressure optimization function, designed specifically for airplane travel. LDAC technology allows you to transfer three times more data (with a maximum transfer rate of 990 Kbit/s) compared to traditional BLUETOOTH® wireless solutions. Thanks to this, you can enjoy high-resolution audio in impeccable quality.'
+  },
+  {  
+    id: 8,
+    name: 'HyperX Cloud Stinger HX-HSCS-BK/EE',
+    price: 25793,
+    rating: 5,
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h88/h69/31714888253470/hyperx-cloud-stinger-hx-hscs-bk-ee-black-red-4803637-1-Container.jpg',
+    link: 'https://kaspi.kz/shop/p/hyperx-cloud-stinger-hx-hscs-bk-ee-chernyi-krasnyi-4803637/?c=750000000#!/item',
+    description: 'HyperX Cloud Stinger™ is the perfect headset for gamers who need a device with low weight, excellent sound quality and exceptional usability. This headset weighs only 275g for exceptional ease of use, and its 90-degree swivel cups allow you to customize the headset for maximum comfort. Directional speakers (50 mm) transmit sound directly to the ear and provide high sound quality. This headset, designed specifically for maximum comfort during long games, uses proprietary high-quality HyperX foam with shape memory. HyperX Cloud Stinger cups with a 90-degree rotation provide a snug fit, which allows you to play comfortably for many hours. The HyperX Cloud Stinger headset weighing only 275 g is practically not felt, which makes it ideal for long-term games. Convenient mute of the microphone by vertical folding towards the head. The built-in HyperX Cloud Stinger headset passive noise reduction function reduces background noise and provides high quality voice transmission.'
+  },
+  {  
+    id: 9,
+    name: 'Marshall Major IV',
+    price: 145830,
+    rating: 5,
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hae/h94/49221104828446/marshall-major-iv-koricnevyj-104024603-3.jpg',
+    link: 'https://kaspi.kz/shop/p/marshall-major-iv-korichnevyi-104024603/?c=750000000#!/item',
+    description: 'The MARSHALL Major IV Bluetooth headset, thanks to its features, will give you a comfortable listening to music of various genres. This model is based on dynamic radiators with a membrane diameter of 40 mm, which together with a wide frequency range and high sensitivity can provide high-quality and rich sound. In addition, the headphones are equipped with a microphone, so they can also be used for telephone conversations. Connection to devices in this model can be carried out both wirelessly and wirelessly – using the energy-efficient Bluetooth 5.0 protocol. Another feature is a large battery that can provide up to 80 hours of playback. Also, MARSHALL Major IV is distinguished by its ease of use. The model has a folding design with an adjustable headband, which will make it easy to choose the optimal position of the headphones. The device is also equipped with soft ear pads made of high-quality materials that will give comfort even with prolonged use. For greater convenience, mechanical playback control buttons are provided on the headphone case.'
+  },
+  {  
+    id: 10,
+    name: 'Apple AirPods Max',
+    price: 378580,
+    rating: 5,
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h0f/h18/33524667154462/apple-airpods-max-serebristyj-100949286-1-Container.jpg',
+    link: 'https://kaspi.kz/shop/p/apple-airpods-max-serebristyi-100949286/?c=750000000#!/item',
+    description: 'The Apple AirPods Max Bluetooth headset will be able to completely change your idea of full-size headphones. This model has been developed using first-class materials and advanced technologies. It is based on the Apple H1 processor and proprietary dynamic drivers, among the advantages of which high operating parameters can be noted. Thanks to this, as well as the support of the spatial audio function and the dynamic head tracking function, the headphones will give you a vivid and unforgettable experience when listening to musical compositions, as if you are in a movie theater. Another important feature of Apple AirPods Max is the support for active noise reduction, thanks to which you can easily abstract from external noise at the touch of a button and concentrate on listening. In addition, the model provides an additional transparent sound mode, in which you can simultaneously clearly hear everything that is happening around you and enjoy music. The Apple AirPods Max Bluetooth headset is made in a metal case and has a stylish design. In addition, this model is distinguished by its ergonomics, which is due to the presence in the design of swivel bowls with soft mesh ear pads and an adjustable headband. To connect to the devices, the Bluetooth 5.0 protocol is used with a range of up to 10 m. The power is supplied from the built-in battery, the operating time of which can reach 20 hours.'
+  },
+
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
